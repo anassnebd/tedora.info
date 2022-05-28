47b0edcf4498e8bf18afceb57f2202cc302bf131
AnassNebdaoui <nebdaoui.anass@gmail.com>
2022-05-28 05:32:45 +0100

    Updates

---
+++ precache-manifest.938a66d1b6a2ee94541f4ed38914f58b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e123f9bfd205f230e04f",
+    "url": "/tedora.info/static/css/main.ff3eb6f5.chunk.css"
+  },
+  {
+    "revision": "e123f9bfd205f230e04f",
+    "url": "/tedora.info/static/js/main.e123f9bf.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/tedora.info/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/tedora.info/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "b14c3318069a11b9f3b0",
+    "url": "/tedora.info/static/js/runtime~main.b14c3318.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tedora.info/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9dc558ec10dfbf2cecee211dfaaca7d7",
+    "url": "/tedora.info/index.html"
+  }
+];
