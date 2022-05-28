822660f9c736cbe656573a01d38af5719ce51686
AnassNebdaoui <nebdaoui.anass@gmail.com>
2022-05-28 05:51:47 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Merci <code>src/App.js</code> and save to reload.
         </p>
